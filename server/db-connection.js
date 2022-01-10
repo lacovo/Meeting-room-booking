319668489c7873f56f76d1e074bc3fdb97f4e779
lacovo <48086085+lacovo@users.noreply.github.com>
2022-01-10 21:27:48 +1300

    Update db-connection.js
    update connection

--- server/db-connection.js
+++ server/db-connection.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 //connect to mysql database from my own server
 var connection = mysql.createConnection({
-  host      : '116.12.63.162',
-  user      : 'NhDdUPjdZn',
-  password  : '8apK7TeDX7',
-  database  : 'test',
+  host      : 'sql6.freemysqlhosting.net',
+  user      : 'sql6464593',
+  password  : 'XGwZM5s8ng',
+  database  : 'sql6464593',
 });
 connection.connect(function(err){
   (err) ? console.log(err): console.log('connected');
